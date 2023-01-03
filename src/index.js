ec87e0dea427c6547f9c7f4d048bbd44b3792986
dankozz1t <alexdankoxxl@gmail.com>
2023-01-03 11:52:37 +0200

    add fonts and variables

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-
-// import 'shared/styles/index.scss';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-// <App />
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-// import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
+
 import App from './App';
 import 'shared/styles/index.scss';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <PersistGate persistor={persistor}>
-            <BrowserRouter>
-                <App />
-                {/* <ToastContainer /> */}
-            </BrowserRouter>
-        </PersistGate>
-    </Provider>
+  // <Provider store={store}>
+  // <PersistGate persistor={persistor}>
+  <BrowserRouter>
+    <App />
+    <ToastContainer />
+  </BrowserRouter>
+  //  </PersistGate>
+  // </Provider>
 );
--- src/pages/SharedLayoutPage/SharedLayoutPage.jsx
+++ src/pages/SharedLayoutPage/SharedLayoutPage.jsx
@@ -5,23 +5,26 @@ import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Container from 'shared/components/Container';
 
-
 const SharedLayoutPage = () => {
-    return (
-        <div>
-            <Suspense fallback={null}>
-            <Container>
-              <h1>I'm ready to start</h1>
-              <p>Wallet Finance App</p>
-             </Container>
-                {/* <Header /> */}
-                <Suspense fallback={null}>
-                    <Outlet />
-                </Suspense>
-            </Suspense>
-        </div>
-    );
+  return (
+    <div>
+      <Suspense fallback={null}>
+        <Container>
+          <h1 style={{ fontFamily: 'var(--title-font) (Poppins)' }}>
+            This is title and I use --title-font
+          </h1>
+          <p style={{ fontFamily: 'var(--main-font) (Circe) ' }}>
+            This is text and I use --main-font
+          </p>
+          <p> This is text and I inherit base font (main-font (Circe) )</p>
+        </Container>
+        {/* <Header /> */}
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </Suspense>
+    </div>
+  );
 };
-
 
 export default SharedLayoutPage;
