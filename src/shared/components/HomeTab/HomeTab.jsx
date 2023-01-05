6682a4e68d22aa27ef45d8895adc7e3b5a2f6133
Olena Yatseniuk <elena.yatsenyuk@gmail.com>
2023-01-05 14:04:45 +0200

    add home table mob

--- src/shared/components/HomeTab/HomeTab.jsx
+++ src/shared/components/HomeTab/HomeTab.jsx
@@ -48,48 +48,82 @@ const HomeTab = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
   return (
     <>
-    {!isMobile &&
-      <div className={s.tableWrap}>
-      <div className={s.scrollTable}>
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Type</th>
-              <th>Category</th>
-              <th>Comment</th>
-              <th>Sum</th>
-              <th>Balance</th>
-            </tr>
-          </thead>
-        </table>
+      {isMobile && (
+        <div className={s.tableWrapMob}>
+          <div className={s.scrollTableMob}>
+            <div className={s.scrollTableBodyMob}>
+              {dataList.map(el => (
+                <table style={{}}>
+                  <tbody>
+                    <tr style={{backgroundColor: "white"}}>
+                      <td>Date</td>
+                      <td>{el.date}</td>
+                    </tr>
+                    <tr style={{backgroundColor: "white", borderRadius: "10px"}}>
+                      <td>Type</td>
+                      <td>{el.type}</td>
+                    </tr>
 
-        <div className={s.scrollTableBody}>
-          <table>
-            <tbody>
-              {
-                  dataList.map(el => (
+                    <tr style={{backgroundColor: "white"}}>
+                      <td>Category</td>
+                      <td>{el.category}</td>
+                    </tr>
+                    <tr style={{backgroundColor: "white"}}>
+                      <td>Comment</td>
+                      <td>{el.comment}</td>
+                    </tr>
+                    <tr style={{backgroundColor: "white"}}>
+                      <td>Sum</td>
+                      <td>{el.sum}</td>
+                    </tr>
+                    <tr style={{backgroundColor: "white"}}>
+                      <td>Balance</td>
+                      <td>{el.balance}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+      {/* desktop and tablet */}
+      {!isMobile && (
+        <div className={s.tableWrap}>
+          <div className={s.scrollTable}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Type</th>
+                  <th>Category</th>
+                  <th>Comment</th>
+                  <th>Sum</th>
+                  <th>Balance</th>
+                </tr>
+              </thead>
+            </table>
+
+            <div className={s.scrollTableBody}>
+              <table>
+                <tbody>
+                  {dataList.map(el => (
                     <tr key={el.comment}>
                       <td>{el.date}</td>
                       <td>{el.type}</td>
-                      <td>
-                        {el.category}
-                      </td>
+                      <td>{el.category}</td>
                       <td>{el.comment}</td>
-                      <td>
-                        {el.sum}
-                      </td>
+                      <td>{el.sum}</td>
                       <td>{el.balance}</td>
                     </tr>
                   ))}
-            </tbody>
-          </table>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-                  }
+      )}
     </>
-
   );
 };
 
