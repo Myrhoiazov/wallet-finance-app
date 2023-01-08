ebfc51b94d5572440b77b27c9da311978c8a8ca0
ArthurKorchak <korchak.arthur@gmail.com>
2023-01-08 22:23:27 +0200

    fix styles

--- src/pages/StatisticsPage/StatisticsPage.jsx
+++ src/pages/StatisticsPage/StatisticsPage.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CustomSelect from 'shared/components/CustomSelect/CustomSelect';
 import DoughnutChart from 'shared/components/DoughnutChart/DoughnutChart';
 import Table from 'shared/components/Table/Table';
 import s from './StatisticsPage.module.scss';
 
+const mockData = [
+  {
+    color: 'green',
+    category: 'Main expenses',
+    value: 8700.0,
+  },
+  {
+    color: 'red',
+    category: 'Products',
+    value: 3800.74,
+  },
+];
+
 const StatisticsPage = () => {
+  const [selectedMonth, setSelectedMonth] = useState(undefined);
+  const [selectedYear, setSelectedYear] = useState(undefined);
+  const [viewData, serViewData] = useState(mockData);
+
   const monthsList = [
     'January',
     'February',
@@ -31,50 +48,38 @@ const StatisticsPage = () => {
     '#81E1FF',
     '#24CCA7',
     '#00AD84',
+    '#AD5C23',
   ];
 
   const yearsList = [2023, 2022, 2021];
 
-  const mockData = [
-    {
-      color: 'green',
-      category: 'Main expenses',
-      value: 8700.0,
-    },
-    {
-      color: 'red',
-      category: 'Products',
-      value: 3800.74,
-    },
-  ];
+  const monthChangeHandle = month => setSelectedMonth(month);
 
-  const monthChangeHandle = event => {
-    console.log(event);
-  };
-
-  const yearChangeHandle = event => {
-    console.log(event);
-  };
+  const yearChangeHandle = year => setSelectedYear(year);
 
   return (
-    <div className={s.mainWrapper}>
-      <DoughnutChart data={mockData} income={100} />
-      <div className={s.tableWrapper}>
-        <div className={s.selectWrapper}>
-          <CustomSelect
-            options={monthsList}
-            changeHandler={monthChangeHandle}
-            name={'month'}
-          />
-          <CustomSelect
-            options={yearsList}
-            changeHandler={yearChangeHandle}
-            name={'year'}
-          />
+    <>
+      {viewData && (
+        <div className={s.mainWrapper}>
+          <DoughnutChart data={mockData} income={100} />
+          <div className={s.tableWrapper}>
+            <div className={s.selectWrapper}>
+              <CustomSelect
+                options={monthsList}
+                changeHandler={monthChangeHandle}
+                name={'month'}
+              />
+              <CustomSelect
+                options={yearsList}
+                changeHandler={yearChangeHandle}
+                name={'year'}
+              />
+            </div>
+            <Table data={mockData} income={100} />
+          </div>
         </div>
-        <Table data={mockData} income={100} />
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
