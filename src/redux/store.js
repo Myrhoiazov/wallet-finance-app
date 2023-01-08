ab19d3a2dc06135adf70cc8536e1873ed71eaa0b
Olena Yatseniuk <elena.yatsenyuk@gmail.com>
2023-01-08 15:40:56 +0200

    delete comment in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -48,20 +48,4 @@ export const store = configureStore({
   devTools: process.env.NODE_ENV === 'development',
 });
 
-// export const store = configureStore({
-//   reducer: {
-//     auth: persistReducer(persistConfig, authReducer),
-//     user: userReducer,
-//     transactions: transactionsReducer,
-//     categories: categoriesReducer,
-//   },
-//   middleware(getDefaultMiddleware) {
-//     return getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }).concat(authErrorLogger);
-//   },
-//   devTools: process.env.NODE_ENV === 'development',
-// });
 export const persistor = persistStore(store);
