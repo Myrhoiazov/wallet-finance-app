7b948ba68e02609da103b9e4d2aa41c37e7b4a68
Myrhoiazov <denis.mirgoyazov@gmail.com>
2023-01-12 13:35:03 +0100

    fix auth styles

--- src/pages/AuthPage/RegisterPage/RegisterPage.jsx
+++ src/pages/AuthPage/RegisterPage/RegisterPage.jsx
@@ -3,35 +3,32 @@ import { MediaQuery } from '../../../hooks/useMediaQuery';
 import s from '../RegisterPage/RegisterPage.module.scss';
 
 const RegisterPage = () => {
-  
-    return (
-      <section className={s.section_register_page}>
-        <MediaQuery.Desktop>
-        <div className={s.section_register_page_wrapper1}>
-                </div>
-                <RegisterForm></RegisterForm>
-              
-                <div className={s.inner_images_bp1280px}>
-                    <div className={s.inner_images_bp1280px__girlLogo}></div>
-                    <h2 className={s.section_register_page_title}>Finance App</h2>
-                  <div className={s.inner_images_bp1280px__ellipseLogo}></div>
-                    </div>
-          </MediaQuery.Desktop>
-        <MediaQuery.Tablet>
+  return (
+    <section className={s.section_register_page}>
+      <MediaQuery.Desktop>
+        <div className={s.section_register_page_wrapper1}></div>
+        <RegisterForm></RegisterForm>
+
+        <div className={s.inner_images_bp1280px}>
+          <div className={s.inner_images_bp1280px__girlLogo}></div>
+          <h2 className={s.section_register_page_title}>Finance App</h2>
+          <div className={s.inner_images_bp1280px__ellipseLogo}></div>
+        </div>
+      </MediaQuery.Desktop>
+      <MediaQuery.Tablet>
         <h2 className={s.section_register_page_title}>Finance App</h2>
         <RegisterForm></RegisterForm>
-              <div className={s.inner_images_bp768px}>
-              <div className={s.inner_images_bp768px__girlLogo}></div>
-                  <div width="618px" className={s.inner_images_bp768px__ellipsePingLogo}></div>
-                  <div className={s.inner_images_bp768px__ellipseLogo}></div>
-              </div>
-          </MediaQuery.Tablet>
-        <MediaQuery.Mobile>
+        <div className={s.inner_images_bp768px}>
+          <div className={s.inner_images_bp768px__girlLogo}></div>
+          <div width="618px" className={s.inner_images_bp768px__ellipsePingLogo}></div>
+          <div className={s.inner_images_bp768px__ellipseLogo}></div>
+        </div>
+      </MediaQuery.Tablet>
+      <MediaQuery.Mobile>
         <RegisterForm></RegisterForm>
-              <div className={s.inner_images_bp320px}></div>
-          </MediaQuery.Mobile>
-      </section>
-
-  )
-}
+        <div className={s.inner_images_bp320px}></div>
+      </MediaQuery.Mobile>
+    </section>
+  );
+};
 export default RegisterPage;
